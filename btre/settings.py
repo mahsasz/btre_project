7617771ae8fc4051838e6f711de9b9919afe35a5
unknown <mahsa.stz@gmail.com>
2020-04-09 17:33:41 -0300

    third commit

--- btre/settings.py
+++ btre/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
+    'listings.apps.ListingsConfig',
+    'realtors.apps.RealtorsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +78,11 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'btredb',
+        'USER': 'postgres',
+        'PASSWORD': '31784161063',
+        'HOST': 'localhost'
     }
 }
 
@@ -119,7 +124,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# the place that it collect all the static from all the apps and put it in this root, when we say python manage.py collectstatic
+# we see that it actually create a static folder with all our static components in it, it should also has admin static in it too
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'btre/static')
 ]
+
+
+# Media folder setting
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
---
+++ listings/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path 
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name = 'listings'), #inja yani safeye index bara listings ine
+    path('<int:listing-id>', views.listing, name = 'listing'),
+    path('search', views.search, name = 'search')
+    # dalile inke inja nemigim listings/search ine ke in gharare link beshe b urls aslimoon va begim k
+    # har chizi ke hast listings/ marboot b in file mishe link mishe be in urls.py dar listings
+]
