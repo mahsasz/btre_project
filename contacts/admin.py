dceef260ed79204401c9760e8ac10c4f3cc8406c
unknown <mahsa.stz@gmail.com>
2020-04-19 20:01:21 -0300

    Final commit

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Contact
+
+class contactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'listing', 'email', 'contact_date')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', 'email', 'listing')
+
+admin.site.register(Contact, contactAdmin)
